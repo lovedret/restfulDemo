206ede1f0426a3bf8e3e9417b085546141e40a71
lovedret <1328786008@qq.com>
2020-08-06 22:16:50 +0800

    xlzhao 0806

---
+++ src/main/java/com/example/demo/GreetController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetController {
+    private static final String template = "Hello, %s!";
+    private AtomicLong id=new AtomicLong();
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "user",defaultValue = "world") String user){
+        Greeting greeting=new Greeting();
+        greeting.setId(id.incrementAndGet());
+        greeting.setUser(String.format(template,user));
+        return greeting;
+    }
+}
---
+++ src/main/java/com/example/demo/Greeting.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class Greeting {
+    private Long id;
+    private String user;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+}
+
