3d04e6adb70bb6ab91161e0db8f3f4fcee6bffb8
lovedret <1328786008@qq.com>
2020-08-07 00:26:17 +0800

    xlzhao 0807

--- src/main/java/com/example/demo/GreetController.java
+++ src/main/java/com/example/demo/GreetController.java
@@ -1,9 +1,6 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -24,5 +21,12 @@ public class GreetController {
         greeting.setId(id.incrementAndGet());
         greeting.setUser(String.format(template,"xlzhao"));
         return greeting;
+    }
+    @RequestMapping("/person/{name}")
+    public Person pathVariableTest(@PathVariable("name") String name){
+        Person person = new Person();
+        person.setAge(25);
+        person.setName(name);
+        return person;
     }
 }
